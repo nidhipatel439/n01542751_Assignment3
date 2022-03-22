1807742b7f07f9014d6996d4f11dad3632c321c7
Nidhi <nidhipatel439@gmail.com>
2022-03-22 11:17:26 -0400

    Add project files.

---
+++ n01542751_Assignment3/Controllers/TeacherDataController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using n01542751_Assignment3.Models;
+using MySql.Data.MySqlClient;
+
+namespace n01542751_Assignment3.Controllers
+{
+    public class TeacherDataController : ApiController
+    {
+        private SchoolDbContext School = new SchoolDbContext();
+
+        [HttpGet]
+        public IEnumerable<string> ListTeacher()
+        {
+            MySqlConnection Conn = School.AccessDatabase();
+
+            Conn.Open();
+
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            cmd.CommandText = "Select * from teachers";
+
+            MySqlDataReader ResultSet = cmd.ExecuteReader();
+
+            List<String> TeacherNames = new List<string> { };
+
+            while (ResultSet.Read())
+            {
+                string teacherName = ResultSet["teacherfname"] + " " + ResultSet["teacherlname"];
+                TeacherNames.Add(teacherName);
+            }
+
+            Conn.Close();
+
+            return TeacherNames;
+
+        }
+    }
+}
---
+++ n01542751_Assignment3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="n01542751_Assignment3.WebApiApplication" Language="C#" %>
